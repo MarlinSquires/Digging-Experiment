3287838f13401a1ee5e65c45dc13f15f384d8221
Drinkaholik <marlinsquires123@gmail.com>
2026-06-24 12:38:19 +0200

    changed some stuff

--- Assets/Terrain/DODTerrainGenerator.cs
+++ Assets/Terrain/DODTerrainGenerator.cs
@@ -3,26 +3,29 @@ using UnityEngine;
 // Voxel-based terrain generator using OOP - highly inefficient
 // Height is determined through perlin noise
 
-public class TerrainGenerator : MonoBehaviour
+public class DODTerrainGenerator : MonoBehaviour
 {
-
-    [SerializeField] private GameObject cubePrefab;
-
     
-    [Header("Grid Values")] 
-    [Tooltip("Length in x-axis")] [Min(1)] [SerializeField] private int gridWidth;
-    [Tooltip("Length in y-axis")] [Min(1)] [SerializeField] private int gridHeight; // length in y axis
-    [Tooltip("Length in z-axis")] [Min(1)] [SerializeField] private int gridLength; // length in z axis
+    [Header("Chunk Values")] 
+    [Tooltip("Number of chunks to spawn")] 
+    [Min(1)] [SerializeField] private int chunkCount = 2;
+    [Tooltip("Length and width - x and z axes")] 
+    [Min(2)] [SerializeField] private int chunkSize = 16;
+    [Tooltip("Height in y-axis")] 
+    [Min(2)] [SerializeField] private int chunkHeight = 16; // length in y axis
+    
     
     [Header("Noise Values")] 
     [Min(0f)] [SerializeField] private float noiseScaleX = 1f;
     [Min(0f)] [SerializeField] private float noiseScaleZ = 1f;
     
+    
     [Header("Materials")] 
-    [SerializeField] private Material snowMaterial;
-    [SerializeField] private Material rockMaterial;
-    [SerializeField] private Material grassMaterial;
-    [SerializeField] private Material waterMaterial;
+    [SerializeField] private Color snowColor;
+    [SerializeField] private Color rockColor;
+    [SerializeField] private Color grassColor;
+    [SerializeField] private Color waterColor;
+    
     
     [Header("Height Values")] 
     [Min(0f)] [SerializeField] private float heightScale;
@@ -44,12 +47,39 @@ public class TerrainGenerator : MonoBehaviour
     [SerializeField] private int waterHeight;
     [Min(0)] [SerializeField] private int waterHeightRange;
 
+    private Mesh _cubeMesh;
 
     private GameObject[] _grid;
+
+    private struct Chunk
+    {
+        public Block[] blocks;
+        public int chunkID;
+
+        public Chunk(int id)
+        {
+            chunkID = id;
+        }
+    }
+
+    private struct Block
+    {
+        public Vector3 position;
+        public BlockType blockType;
+    }
+
+    private enum BlockType
+    {
+        Snow,
+        Rock,
+        Grass,
+        Water
+    }
     
     
     void Start()
     {
+        _cubeMesh = Resources.GetBuiltinResource<Mesh>("Cube.fbx");
         GenerateTerrain();
     }
 
@@ -74,22 +104,22 @@ public class TerrainGenerator : MonoBehaviour
         }
         
         
-        var arrayLength = gridLength * gridWidth * gridHeight;
+        var arrayLength = gridLength * chunkWidth * chunkHeight;
         _grid = new GameObject[arrayLength];
 
-        for (int x = 0; x < gridWidth; x++)
+        for (int x = 0; x < chunkWidth; x++)
         {
             for (int z = 0; z < gridLength; z++)
             {
                 // Determine height based on perlin noise
-                float noiseVal = Mathf.PerlinNoise((float)x / gridWidth * noiseScaleX, (float)z / gridLength * noiseScaleZ);
+                float noiseVal = Mathf.PerlinNoise((float)x / chunkWidth * noiseScaleX, (float)z / gridLength * noiseScaleZ);
                 int height = (int)(noiseVal * heightScale);
                 
-                if (height > gridHeight) height = gridHeight; 
+                if (height > chunkHeight) height = chunkHeight; 
                 
                 for (int y = 0; y < height; y++)
                 {
-                    int index = x + (z * gridLength) + (y * gridLength * gridWidth);
+                    int index = x + (z * gridLength) + (y * gridLength * chunkWidth);
                     
                     _grid[index] = Instantiate(cubePrefab, new Vector3(x, y, z), Quaternion.identity);
                     
